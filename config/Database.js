c756c9ff9aeb212450ec441deb40d8fffe7a70f2
ara fa adri <arafa.sco@gmail.com>
2023-09-25 10:28:20 +0700

    update git ignore

---
+++ config/Database.js
@@ -0,0 +1,13 @@
+const sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = new sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD,{
+    host:process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT
+});
+
+module.exports = db;
+
+//uji coba git ke server
